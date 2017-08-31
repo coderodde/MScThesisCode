7fde6220921fb85c043a03578da122bec9c591f0
coderodde <rodion.efremov@cs.helsinki.fi>
2017-08-31 14:25:40 +0300

    Working on file IO.

--- src/net/coderodde/msc/Main.java
+++ src/net/coderodde/msc/Main.java
@@ -1,98 +1,88 @@
 package net.coderodde.msc;
 
-import net.coderodde.msc.support.DNAAlphabet;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
-import net.coderodde.msc.support.BasicParsimoniousContextTreeLearner;
-//import net.coderodde.msc.support.BasicParsimoniousContextTreeLearnerV2;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<DataRow<Character>> dataRowList = new ArrayList<>();
-        DNAAlphabet alphabet = new DNAAlphabet();
+        if (args.length != 3) {
+            printHelpMessage(System.out);
+            return;
+        }
         
-        // test_file.txt (same score, same tree)
-//        dataRowList.add(new DataRow<Character>('A', 'A'));
-//        dataRowList.add(new DataRow<Character>('T', 'A'));
-//        dataRowList.add(new DataRow<Character>('A', 'A'));
-//        dataRowList.add(new DataRow<Character>('C', 'G'));
-//        dataRowList.add(new DataRow<Character>('C', 'T'));
-//        dataRowList.add(new DataRow<Character>('C', 'A'));
-//        dataRowList.add(new DataRow<Character>('T', 'A'));
-//        dataRowList.add(new DataRow<Character>('T', 'A'));
-//        dataRowList.add(new DataRow<Character>('T', 'C'));
-//        dataRowList.add(new DataRow<Character>('C', 'T'));
-       
-        AbstractParsimoniousContextTreeLearner<Character> learner1 =
-                new BasicParsimoniousContextTreeLearner<>();
-
-        // test_file_2.txt (same score, same tree)
-//        dataRowList.add(new DataRow<Character>('A', 'A', 'C'));
-//        dataRowList.add(new DataRow<Character>('A', 'C', 'C'));
-//        dataRowList.add(new DataRow<Character>('A', 'T', 'C'));
-//        dataRowList.add(new DataRow<Character>('T', 'A', 'C'));
-//        dataRowList.add(new DataRow<Character>('T', 'T', 'G'));
-//        dataRowList.add(new DataRow<Character>('G', 'A', 'C'));
-//        dataRowList.add(new DataRow<Character>('C', 'C', 'T'));
-//        dataRowList.add(new DataRow<Character>('C', 'A', 'T'));
-//        dataRowList.add(new DataRow<Character>('C', 'G', 'G'));
-//        dataRowList.add(new DataRow<Character>('T', 'C', 'A'));
-//        dataRowList.add(new DataRow<Character>('C', 'G', 'T'));
-//        dataRowList.add(new DataRow<Character>('C', 'C', 'A'));
-
-        // test_file_3.txt (same score, same trees)
-//        dataRowList.add(new DataRow<Character>('C', 'T', 'T', 'A'));
-//        dataRowList.add(new DataRow<Character>('G', 'T', 'C', 'C'));
-//        dataRowList.add(new DataRow<Character>('A', 'T', 'C', 'T'));
-//        dataRowList.add(new DataRow<Character>('G', 'G', 'A', 'T'));
-//        dataRowList.add(new DataRow<Character>('T', 'T', 'C', 'T'));
-//        dataRowList.add(new DataRow<Character>('A', 'A', 'G', 'A'));
-
-        // test_file_3b.txt (same score, different trees)
-//        dataRowList.add(new DataRow<Character>('C', 'T', 'T', 'A'));
-//        dataRowList.add(new DataRow<Character>('G', 'T', 'C', 'C'));
-//        dataRowList.add(new DataRow<Character>('G', 'T', 'C', 'T'));
-//        dataRowList.add(new DataRow<Character>('G', 'G', 'C', 'T'));
-//        dataRowList.add(new DataRow<Character>('T', 'T', 'C', 'T'));
-//        dataRowList.add(new DataRow<Character>('A', 'A', 'G', 'A'));
-
-        // test_file_3c.txt (same score, different trees)
-//        dataRowList.add(new DataRow<Character>('A', 'C', 'C', 'A'));
-//        dataRowList.add(new DataRow<Character>('T', 'A', 'A', 'G'));
-//        dataRowList.add(new DataRow<Character>('G', 'G', 'T', 'T'));
-//        dataRowList.add(new DataRow<Character>('T', 'C', 'G', 'T'));
-//        dataRowList.add(new DataRow<Character>('A', 'T', 'G', 'C'));
-
-        // test_file_4.txt (Disagrees with Ralf!) Fixed!
-        dataRowList.add(new DataRow<>('A', 'C', 'T'));
-        dataRowList.add(new DataRow<>('T', 'A', 'A'));
-        dataRowList.add(new DataRow<>('G', 'C', 'C'));
-        dataRowList.add(new DataRow<>('T', 'C', 'G'));
-        dataRowList.add(new DataRow<>('C', 'C', 'T'));
-        dataRowList.add(new DataRow<>('C', 'A', 'A'));
+        int depth;
         
-        // test_file_4b.txt (takes eternity).
-//        dataRowList.add(new DataRow<Character>('A', 'C', 'C', 'G', 'T'));
-//        dataRowList.add(new DataRow<Character>('T', 'T', 'C', 'G', 'A'));
-//        dataRowList.add(new DataRow<Character>('G', 'C', 'C', 'A', 'C'));
-//        dataRowList.add(new DataRow<Character>('T', 'T', 'C', 'G', 'G'));
-//        dataRowList.add(new DataRow<Character>('C', 'A', 'C', 'C', 'T'));
-//        dataRowList.add(new DataRow<Character>('C', 'G', 'A', 'T', 'A'));
-
-        long a = System.currentTimeMillis();
-//        ParsimoniousContextTree<Character> debugTree = 
-//                new BasicParsimoniousContextTreeLearnerV2<Character>()
-//                        .learn(alphabet, dataRowList);
-        long b = System.currentTimeMillis();
-//        System.out.println(debugTree);
-        System.out.println("Debug tree in " + (b - a) + " ms.");
-        long startTime = System.currentTimeMillis();
-        ParsimoniousContextTree<Character> tree = learner1.learn(dataRowList);
-        long endTime = System.currentTimeMillis();
+        try {
+            depth = Integer.parseInt(args[2]);
+        } catch (NumberFormatException ex) {
+            System.err.println(args[2] + ": not an integer.");
+            System.exit(1);
+        }
         
-        System.out.println("Computed in " + (endTime - startTime) + " ms.");
-        System.out.println(tree);
-//        System.out.println("debugTree score: " + debugTree.getScore());
+        int start;
+        
+        try {
+            start = Integer.parseInt(args[1]);
+        } catch (NumberFormatException ex) {
+            System.err.println(args[1] + ": not an integer.");
+            System.exit(1);
+        }
+        
+        File file = new File(args[0]);
+        checkFile(file);
+        List<DataRow<Character>> dataRows = new ArrayList<>();
+        
+        try (Scanner scanner = new Scanner(file, "UTF-8")) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim().toLowerCase();
+                Character[] arr = new Character[line.length()];
+                
+                int i = 0;
+
+                for (char c : line.toCharArray()) {
+                    arr[i++] = c;
+                }
+                
+                dataRows.add(new DataRow<Character>(arr));
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.err);
+            System.err.println("File not found!");
+            System.exit(0);
+        }
+        
+        for (DataRow<Character> dataRow : dataRows) {
+            System.out.println(dataRow);
+        }
+    }
+    
+    private static void checkFile(File file) {
+        if (!file.exists()) {
+            System.err.println("The file \"" + file.getAbsolutePath() + "\" " +
+                               "does not exist.");
+            System.exit(1);
+        }
+        
+        if (!file.isFile()) {
+            System.err.println("The file \"" + file.getAbsolutePath() + "\" " +
+                               "is not a regular file.");
+            System.exit(1);
+        }
+    }
+    
+    private static void printHelpMessage(PrintStream out) {
+        out.println("usage: java -jar YourJar.jar FILE START DEPTH");
+        out.println("Where:");
+        out.println("  FILE  " + 
+                    "is the name of the file containing the input data.");
+        out.println("  START " +
+                    "is the starting index of the explanatory variable.");
+        out.println("  DEPTH is the number of explanatory variables/" +
+                    "the depth of the PCT tree.");
     }
 }
