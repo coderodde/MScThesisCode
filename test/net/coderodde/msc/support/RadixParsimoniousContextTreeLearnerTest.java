6dc235a67278590fd820d7ccd71f2a8c9ba7edf2
Rodion Efremov <rodion.efremov@helsinki.fi>
2018-01-02 20:38:57 +0200

    Need to test the radix PCT learner.

---
+++ test/net/coderodde/msc/support/RadixParsimoniousContextTreeLearnerTest.java
@@ -0,0 +1,18 @@
+package net.coderodde.msc.support;
+
+import net.coderodde.msc.ParsimoniousContextTree;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RadixParsimoniousContextTreeLearnerTest {
+    
+    @Test
+    public void test() {
+//        RadixParsimoniousContextTreeLearner<C> learner = 
+//                new RadixParsimoniousContextTreeLearner<>()
+    }
+}
